e138bc2eb29fc00782a1fa6bb997311c5a77bc29
Derek Pease <dcpeaz@gmail.com>
2023-11-29 18:36:02 -0500

    Week 3 Homework

--- week3homework.js
+++ week3homework.js
@@ -2,32 +2,38 @@ const pizzaToppings = ["pepperoni", "bacon", "sausage", "ham"];
 
 function greetCustomer() {
   console.log("Welcome to Peaza Pizza, our available toppings are:");
-  for (let pizzaTopping of pizzaToppings)
-  console.log(pizzaTopping);
-};
-
+  for (let pizzaTopping of pizzaToppings) console.log(pizzaTopping);
+}
 
 function getPizzaOrder(size, crust, ...toppings) {
-  console.log("One " + size + crust + " crust pizza with " + toppings + " coming right up!");
-  const pizzaOrder = {
-    size: size,
-    crust: crust,
-    toppings: toppings
-  };
-  return pizzaOrder
-};
-
+  console.log(
+    `One ${size} ${crust} crust pizza with ${toppings} coming right up!`
+  );
+  const pizzaOrder = [size, crust, toppings];
+  return pizzaOrder;
+}
 
 function preparePizza(details) {
   const [size, crust, toppings] = details;
   console.log("...cooking pizza...");
-  const pizza = {
+  const cookPizza = {
     size: size,
     crust: crust,
     toppings: toppings
   };
-return pizza
-};
+  return cookPizza;
+}
 
+function servePizza(cookPizza) {
+  console.log(
+    `Pizza's ready! Here's your ${cookPizza.size} ${
+      cookPizza.crust
+    } crust pizza with ${cookPizza.toppings.join(", ")}. Enjoy!`
+  );
+  return cookPizza;
+}
 
-function servePizza()
+greetCustomer();
+const customerOrder = getPizzaOrder("Large", "Thin", "Pepperoni", "Sausage");
+const finishedPizza = preparePizza(customerOrder);
+servePizza(finishedPizza);
